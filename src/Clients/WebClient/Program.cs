0125d52d7a5f4f8c73658dac0873a49f7d339898
xpointer <xpointer@pm.me>
2022-07-14 09:48:28 -0700

    Init project

---
+++ src/Clients/WebClient/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using FeedLooker.WebClient;
+
+var builder = WebAssemblyHostBuilder.CreateDefault();
+builder.RootComponents.Add<App>("#app");
+
+var host = builder.Build();
+await host.RunAsync();
